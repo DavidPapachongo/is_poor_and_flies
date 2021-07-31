e437fbd5dee5c600828026036720025ec0e621a0
David Salazar <davidjosesalazarmora@gmail.com>
2021-07-31 09:22:26 -0500

    cambios en get_song y create_upload_file

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import glob
 import os
 from models import Music
 from database import SessionLocal, engine
+from sqlalchemy.orm.exc import NoResultFound
 import eyed3
 from starlette.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -34,25 +35,28 @@ def get_mp3_metadata(filepath):
 
 @app.post("/uploadfile/", status_code=status.HTTP_201_CREATED)
 def create_upload_file(file: UploadFile = File(...)):
-    os.chdir(path_mp3)
-    buffer = open(file.filename, "wb")
-    shutil.copyfileobj(file.file, buffer)
+    print(file.filename)
+    if file.filename.endswith(".mp3"):
+        os.chdir(path_mp3)
+        buffer = open(file.filename, "wb")
+        shutil.copyfileobj(file.file, buffer)
 
-    value = get_mp3_metadata(file.filename)
+        value = get_mp3_metadata(file.filename)
 
-    with SessionLocal() as db:
-        music = Music(
-            filename=value["song"],
-            title=value["title"],
-            artist=value["artist"],
-            album=value["album"],
-            year=value["year"]
-        )
-        db.add(music)
-        db.commit()
-        db.refresh(music)
-
-    return value
+        with SessionLocal() as db:
+            music = Music(
+                filename=value["song"],
+                title=value["title"],
+                artist=value["artist"],
+                album=value["album"],
+                year=value["year"]
+            )
+            db.add(music)
+            db.commit()
+            db.refresh(music)
+        return value
+    else:
+        raise HTTPException(status_code=400, detail="invalid file")
 
 
 @app.get("/songs/", status_code=200)
@@ -65,10 +69,11 @@ def get_songs():
 @app.get("/songs/{song_id}", status_code=200)
 def get_song(song_id: int):
     with SessionLocal() as db:
-        song = db.query(Music).filter(Music.id == song_id).one()
-        if song is None:
+        try:
+            song = db.query(Music).filter(Music.id == song_id).one()
+            return song
+        except NoResultFound:
             raise HTTPException(status_code=404, detail="music not found")
-        return song
 
 
 @app.get("/songs/{song_id}/file", status_code=200)
@@ -94,6 +99,7 @@ def like_a_song(song_id: int):
             db.commit()
             db.refresh(song)
             return song
+
 
 origins = os.environ["ALLOW_ORIGINS"].split(",")
 
